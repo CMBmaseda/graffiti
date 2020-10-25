c8691d3457ea19c524cc042375d4e0b1c8fd210c
Tony Maseda <ajmaseda@gmail.com>
2020-10-25 14:23:36 -0400

    Replaced fontawesome link

--- wp-content/themes/graffiti/header.php
+++ wp-content/themes/graffiti/header.php
@@ -18,7 +18,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript" src="//fast.fonts.net/jsapi/1504234f-c647-4850-97c8-bb98b8733769.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<script src="https://use.fontawesome.com/f7e19a4b65.js"></script>
 <link href='https://fonts.googleapis.com/css?family=Abril+Fatface|Quicksand' rel='stylesheet' type='text/css'>
 
 <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
